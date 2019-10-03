d7b73ebb0498358f48af6e84dcc60d3fade43201
Devin Warrick <dev.warr@gmail.com>
2019-10-03 13:30:23 -0500

    Add info(unstyled) to the Display Paper

--- baseball-stadium/src/components/Display.js
+++ baseball-stadium/src/components/Display.js
@@ -1,23 +1,59 @@
 import React from "react"
 import { BaseBallPlayerHit, BaseBallPlayerReady } from "./SVG"
-import { Container } from "@material-ui/core"
+import { Container, Paper } from "@material-ui/core"
 import { makeStyles } from "@material-ui/styles"
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     root: {
-        marginTop: "25px"
+        margin: "25px auto"
+    },
+    display: {
+        display: "flex"
     }
-})
+}))
 
 export default function Display(props) {
-    const classes = useStyles();
-
+    const classes = useStyles()
 
     return (
-        <Container className={classes.root}>
-            <Container maxWidth="sm">
+        <Container maxWidth="md" className={classes.root}>
+            <Paper className={classes.display}>
+                <Container>
+                    <h2>BATTER</h2>
+                    <p>
+                        {props.batter ? props.batter.name || "Jhonny" : "Ricky"}
+                    </p>
+                    <div>
+                        <h4>STATS:</h4>
+                        <ul>
+                            {props.batter
+                                ? props.batter.stats
+                                    ? props.batter.stats.map(stat => (
+                                          <li>{stat}</li>
+                                      ))
+                                    : "No Stats Found"
+                                : "None found"}
+                        </ul>
+                    </div>
+                </Container>
                 {true ? <BaseBallPlayerHit /> : <BaseBallPlayerReady />}
-            </Container>
+                <Container>
+                    {props.play
+                        ? props.play.map(infoBit => (
+                              <div>
+                                  <p>
+                                      {infoBit.name}{" "}
+                                      <span>{infoBit.number}</span>
+                                  </p>
+                              </div>
+                          ))
+                        : "Play info not created yet."}
+                    <div>
+                        <h3>Ball Speed</h3>
+                        <p>{props.ball ? props.ball.speed : "No info"}</p>
+                    </div>
+                </Container>
+            </Paper>
         </Container>
     )
 }
