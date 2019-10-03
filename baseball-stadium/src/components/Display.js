34e6da3488f8b5a360e8a6dddb7b6e75392814aa
Devin Warrick <dev.warr@gmail.com>
2019-10-03 12:22:19 -0500

    Import SVGs inside img tags, add prettier file

---
+++ baseball-stadium/src/components/Display.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BaseBallPlayerHit, BaseBallPlayerReady } from "./SVG"
+
+export default function Display() {
+    return (
+        <div>
+            <BaseBallPlayerHit/>
+            <BaseBallPlayerReady/>
+        </div>
+    )
+}
