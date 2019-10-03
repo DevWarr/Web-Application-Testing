224d0b3d1389852664627c293b25d446554e4cc5
Devin Warrick <dev.warr@gmail.com>
2019-10-03 13:04:33 -0500

    Convert Dashboard to an expansion panel

--- baseball-stadium/src/components/Dashboard.js
+++ baseball-stadium/src/components/Dashboard.js
@@ -1,15 +1,32 @@
-import React from "react";
-import { Button, ButtonGroup, Container } from "@material-ui/core"
+import React from "react"
+import {
+    Button,
+    ButtonGroup,
+    Container,
+    ExpansionPanel,
+    ExpansionPanelSummary,
+    ExpansionPanelDetails
+} from "@material-ui/core"
+import { ExpandMore as ExpandMoreIcon } from "@material-ui/icons"
+
+
 
 export default function DashBoard(props) {
     return (
-        <Container maxWidth="md">
-            <ButtonGroup color="primary">
-                <Button>HIT</Button>
-                <Button>FOUL</Button>
-                <Button>STRIKE</Button>
-                <Button>BALL</Button>
-            </ButtonGroup>
+        <Container maxWidth="sm">
+            <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    Dashboard
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <ButtonGroup color="primary">
+                        <Button>HIT</Button>
+                        <Button>FOUL</Button>
+                        <Button>STRIKE</Button>
+                        <Button>BALL</Button>
+                    </ButtonGroup>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
         </Container>
     )
 }
--- baseball-stadium/src/components/Display.js
+++ baseball-stadium/src/components/Display.js
@@ -1,10 +1,20 @@
 import React from "react"
 import { BaseBallPlayerHit, BaseBallPlayerReady } from "./SVG"
 import { Container } from "@material-ui/core"
+import { makeStyles } from "@material-ui/styles"
+
+const useStyles = makeStyles({
+    root: {
+        marginTop: "25px"
+    }
+})
 
 export default function Display(props) {
+    const classes = useStyles();
+
+
     return (
-        <Container>
+        <Container className={classes.root}>
             <Container maxWidth="sm">
                 {true ? <BaseBallPlayerHit /> : <BaseBallPlayerReady />}
             </Container>
