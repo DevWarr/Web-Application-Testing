498f2d947db98be9f6923ef68a7c3f9eeeecc719
Devin Warrick <dev.warr@gmail.com>
2019-10-04 01:40:53 -0500

    Add 'Miss' SVG

--- baseball-stadium/src/components/SVG/index.js
+++ baseball-stadium/src/components/SVG/index.js
@@ -1,2 +1,3 @@
 export { default as BaseBallPlayerHit } from "./BaseBallPlayerHit"
 export { default as BaseBallPlayerReady } from "./BaseBallPlayerReady"
+export { default as BaseBallPlayerMiss } from "./BaseBallPlayerMiss"
