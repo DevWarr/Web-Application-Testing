dc5f57c3df61b6cc7a81f3478d6357ed2eab6463
Devin Warrick <dev.warr@gmail.com>
2019-10-04 01:43:06 -0500

    Add integration test into App.test.js; add extra logic in 'updatePlay' inside App.js

--- baseball-stadium/src/App.js
+++ baseball-stadium/src/App.js
@@ -8,29 +8,75 @@ function App() {
         stats: ["He sucks."]
     })
     const [play, setPlay] = useState({
-        strike: { total: 3, current: 0 },
-        ball: { total: 4, current: 0 },
-        out: { total: 3, current: 0 }
+        strikes: { total: 3, current: 0 },
+        balls: { total: 4, current: 0 },
+        outs: { total: 3, current: 0 }
     })
-    const [ball, setBall] = useState(0)
 
+    /**
+     * Updates the scoreboard depending on what `type` of play was made
+     * @param type - A string (ex: `strike`, `hit`) referencing the type of play the batter made
+     */
     const updatePlay = type => {
-        if (type === "foul") {
-            if (play.strike.current >= 2) {
-                return
-            } else {
-                type = "strike"
-            }
-        }
-        console.log(type)
+        // Create and object copy, mutate it, then set it at the bottom of the function
         const nextPlay = Object.assign({}, play);
-        nextPlay[type].current++;
+
+        /**
+         * Codes:
+         * 0 = nothing
+         * 1 = ball -> batter moves one base
+         * 2 = hit! -> batter runs to the bases
+         * -1 = strike! -> batter is out!
+         * -2 = outs! -> Team swaps!
+         */
+        let batter = 0;
+
+
+        switch(type) {
+            case "strikes":
+                nextPlay.strikes.current++;
+                if (nextPlay.strikes.current > 2) {
+                    nextPlay.strikes.current = 0
+                    nextPlay.balls.current = 0
+                    nextPlay.outs.current++
+                    batter = -1
+                }
+                if (nextPlay.outs.current > 2) {
+                    nextPlay.strikes.current = 0
+                    nextPlay.balls.current = 0
+                    nextPlay.outs.current = 0
+                    batter = -2
+                }
+                break;
+            case "balls":
+                nextPlay.balls.current++;
+                if (nextPlay.balls.current > 3) {
+                    nextPlay.strikes.current = 0
+                    nextPlay.balls.current = 0
+                    batter = 1
+                }
+                break;
+            case "hit":
+                nextPlay.strikes.current = 0
+                nextPlay.balls.current = 0
+                batter = 2
+                break;
+            case "foul":
+                if (play.strikes.current < 2) {
+                    nextPlay.strikes.current++
+                    break;
+                }
+                break;
+            default:
+                break;
+        }
+        // TODO: Add logic so batter success/failure is displayed somewhere
         setPlay(nextPlay);
     }
 
     return (
         <div className="App">
-            <Display batter={batter} play={play} ball={ball} />
+            <Display batter={batter} play={play} />
             <Dashboard updatePlay={updatePlay} />
         </div>
     )
--- baseball-stadium/src/App.test.js
+++ baseball-stadium/src/App.test.js
@@ -1,9 +1,116 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import * as rtl from "@testing-library/react";
+import { display } from "@material-ui/system";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+test("<App/> renders without crashing", () => {
+    const div = document.createElement("div")
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+})
+
+// Integration testing
+test(`<App/> - When calling "updatePlay()", everything updates properly`, async () => {
+    const app = rtl.render(<App/>);
+
+    // Our views
+    const strikes = app.getByText(/strikes/i)
+    const balls = app.getByText(/balls/i)
+    const outs = app.getByText(/outs/i)
+
+    // Our buttons
+    const btnHit = app.getByTestId("hit")
+    const btnStrike = app.getByTestId("strike")
+    const btnFoul = app.getByTestId("foul")
+    const btnBall = app.getByTestId("ball")
+
+    // Testing initial State
+    expect(strikes.textContent).toBe("STRIKES 0")
+    expect(balls.textContent).toBe("BALLS 0")
+    expect(outs.textContent).toBe("OUTS 0")
+    expect(btnHit).toBeDefined()
+    expect(btnStrike).toBeDefined()
+    expect(btnFoul).toBeDefined()
+    expect(btnBall).toBeDefined()
+
+    // Click on Strike - Strike increases
+    rtl.fireEvent.click(btnStrike)
+    expect(strikes.textContent).toBe("STRIKES 1")
+    expect(balls.textContent).toBe("BALLS 0")
+    expect(outs.textContent).toBe("OUTS 0")
+    
+    // Click on Foul - Strike increases
+    rtl.fireEvent.click(btnFoul)
+    expect(strikes.textContent).toBe("STRIKES 2")
+    expect(balls.textContent).toBe("BALLS 0")
+    expect(outs.textContent).toBe("OUTS 0")
+
+    // Click on Foul a second time (should be no change)
+    rtl.fireEvent.click(btnFoul)
+    expect(strikes.textContent).toBe("STRIKES 2")
+    expect(balls.textContent).toBe("BALLS 0")
+    expect(outs.textContent).toBe("OUTS 0")
+    
+    // Click on Ball - ball increases
+    rtl.fireEvent.click(btnBall)
+    expect(strikes.textContent).toBe("STRIKES 2")
+    expect(balls.textContent).toBe("BALLS 1")
+    expect(outs.textContent).toBe("OUTS 0")
+
+    // Click on Ball Twice more . . .
+    rtl.fireEvent.click(btnBall)
+    rtl.fireEvent.click(btnBall)
+    expect(strikes.textContent).toBe("STRIKES 2")
+    expect(balls.textContent).toBe("BALLS 3")
+    expect(outs.textContent).toBe("OUTS 0")
+
+    // Final Ball should reset strikes and balls (batter goes to 1st base)
+    rtl.fireEvent.click(btnBall)
+    expect(strikes.textContent).toBe("STRIKES 0")
+    expect(balls.textContent).toBe("BALLS 0")
+    expect(outs.textContent).toBe("OUTS 0")
+    
+    // Foul test! - Should be strikes = 1
+    rtl.fireEvent.click(btnFoul)
+    expect(strikes.textContent).toBe("STRIKES 1")
+    expect(balls.textContent).toBe("BALLS 0")
+    expect(outs.textContent).toBe("OUTS 0")
+
+    // Ball test! - Should be Balls = 1
+    rtl.fireEvent.click(btnBall)
+    expect(strikes.textContent).toBe("STRIKES 1")
+    expect(balls.textContent).toBe("BALLS 1")
+    expect(outs.textContent).toBe("OUTS 0")
+    
+    // Strike! . . .
+    rtl.fireEvent.click(btnStrike)
+    expect(strikes.textContent).toBe("STRIKES 2")
+    expect(balls.textContent).toBe("BALLS 1")
+    expect(outs.textContent).toBe("OUTS 0")
+    
+    // Strikeout!! Strikes and Balls reset, and outs increases by 1
+    rtl.fireEvent.click(btnStrike)
+    expect(strikes.textContent).toBe("STRIKES 0")
+    expect(balls.textContent).toBe("BALLS 0")
+    expect(outs.textContent).toBe("OUTS 1")
+    
+    // Final test: If we strikeout twice more, everything should reset to zero:
+    rtl.fireEvent.click(btnStrike) // 1
+    rtl.fireEvent.click(btnStrike) // 2
+    rtl.fireEvent.click(btnStrike) // 3 - OUT!
+    expect(strikes.textContent).toBe("STRIKES 0")
+    expect(balls.textContent).toBe("BALLS 0")
+    expect(outs.textContent).toBe("OUTS 2")
+    rtl.fireEvent.click(btnStrike) // 1
+    rtl.fireEvent.click(btnStrike) // 2 . . .
+    expect(strikes.textContent).toBe("STRIKES 2")
+    expect(balls.textContent).toBe("BALLS 0")
+    expect(outs.textContent).toBe("OUTS 2")
+    rtl.fireEvent.click(btnStrike) // 3 - OUTT!!
+    expect(strikes.textContent).toBe("STRIKES 0")
+    expect(balls.textContent).toBe("BALLS 0")
+    expect(outs.textContent).toBe("OUTS 0")
+    
+})
